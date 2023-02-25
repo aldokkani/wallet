9ef5cdd29741f4ef6e59194539d0d944b0d097f9
Hossam Aldokkani <hossam.aldokkani@musala.com>
2023-02-25 10:33:13 +0200

    feat: define routines

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import morgan from 'morgan'
+import authRouter from './routers/auth'
+import userRouter from './routers/user'
+import walletRouter from './routers/wallet'
 
 const app = express()
 const LOG_FORMAT = process.env['LOG_FORMAT'] ?? 'dev'
 
 app.use(morgan(LOG_FORMAT))
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
-})
+app.use('auth', authRouter)
+app.use('users', userRouter)
+app.use('wallet', walletRouter)
 
 export default app
